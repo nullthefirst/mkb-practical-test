1bb6b38261a5d31d6ee6477cdd9eb143474d4bbd
Ninte <ninte.dev@gmail.com>
2021-08-17 15:36:54 +0100

    Components entrypoint.

--- src/practical/index.js
+++ src/practical/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import Reset from './Reset';
+import Login from './Login';
+import CreateProfile from './CreateProfile';
+import OtpValidation from './OtpValidation';
 
 export default function Practical() {
   return (
     <div>
       <Reset />
+      <hr />
+      <Login />
+      <hr />
+      <CreateProfile />
+      <hr />
+      <OtpValidation />
     </div>
   );
 }
